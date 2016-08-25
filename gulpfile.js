540f3a6fed9967c25e2164df15dc951e2034f74c
hasankryeziusrf <hasan.kryeziu@srf.ch>
2016-08-25 17:29:10 +0200

    build with gulp
    srfcms-4232

--- gulpfile.js
+++ gulpfile.js
@@ -1,142 +1,73 @@
 'use strict';
 
-var gulp            = require('gulp');
-var gulpLoadPlugins = require('gulp-load-plugins');
-var del             = require('del');
-var runSequence     = require('run-sequence');
-var browserSync     = require('browser-sync');
-var path            = require('path');
-var merge           = require('merge-stream');
+var gulp            = require('gulp'),
+    gulpLoadPlugins = require('gulp-load-plugins'),
+    del             = require('del'),
+    runSequence     = require('run-sequence'),
+    browserSync     = require('browser-sync'),
+    path            = require('path'),
+    shell           = require('gulp-shell');
 
 var $ = gulpLoadPlugins();
 var reload = browserSync.reload;
 
-const DEST = 'dist';
+
+const DEST = 'public';
 const AUTOPREFIXER_BROWSERS = [
-  'ie >= 10',
-  'ie_mob >= 10',
-  'ff >= 30',
-  'chrome >= 34',
-  'safari >= 7',
-  'opera >= 23',
-  'ios >= 7',
-  'android >= 4.4',
-  'bb >= 10'
+    'ie >= 10',
+    'ie_mob >= 10',
+    'ff >= 30',
+    'chrome >= 34',
+    'safari >= 7',
+    'opera >= 23',
+    'ios >= 7',
+    'android >= 4.4',
+    'bb >= 10'
 ];
-
-var distpath = function(subpath) {
-  return !subpath ? DEST : path.join(DEST, subpath);
-};
-
-gulp.task('styles', function () {
-  return gulp.src([
-          'app/assets/scss/**/*.scss'
-      ])
-      .pipe($.sourcemaps.init())
-      .pipe($.sass.sync({
-          outputStyle: 'compressed',    //use 'nested' for debugging
-          precision: 10,
-      }).on('error', $.sass.logError))
-      .pipe($.autoprefixer({browsers: AUTOPREFIXER_BROWSERS}))
-      .pipe($.sourcemaps.write('./maps'))
-      .pipe(gulp.dest(distpath('assets/css')))
-      .pipe($.size({
-          title: 'css'
-      }));;
-});
-
-gulp.task('images', function() {
-  return gulp.src([
-          'app/assets/img/**'
-      ])
-      .pipe($.imagemin({
-          progressive: true,
-          interlaced: true
-      }))
-      .pipe(gulp.dest(distpath('assets/img')))
-      .pipe($.size({
-          title: 'images'
-      }));
-});
-
-gulp.task('fonts', function() {
-  return gulp.src([
-          'app/assets/fonts/**'
-      ])
-      .pipe(gulp.dest(distpath('assets/fonts')))
-      .pipe($.size({
-          title: 'fonts'
-      }));
-});
-
-gulp.task('vulcanize', function() {
-    return gulp.src('app/elements/elements.html')
-        .pipe($.vulcanize({
-            stripComments: true,
-            inlineCss: true,
-            inlineScripts: true
-        }))
-        .pipe(gulp.dest(distpath('elements')))
-        .pipe($.size({
-            title: 'vulcanize'
-        }));
-});
-
-gulp.task('copy', function() {
-  var app = gulp.src([
-    'app/*',
-    '!app/assets',
-    '!app/test',
-    '!app/elements',
-    '!app/bower_components',
-    '!app/cache-config.json',
-    '!**/.DS_Store'
-  ], {
-    dot: true
-  }).pipe(gulp.dest(distpath()));
-
-  // Copy over only the bower_components we need
-  // These are things which cannot be vulcanized
-  var bower = gulp.src([
-    'app/bower_components/{webcomponentsjs,platinum-sw,sw-toolbox,promise-polyfill}/**/*'
-  ]).pipe(gulp.dest(distpath('bower_components')));
-
-  return merge(app, bower)
-      .pipe($.size({
-        title: 'copy'
-      }));
-});
-
-gulp.task('serve', function() {
-  browserSync({
-    notify: false,
-    port: 3000,
-    server: {
-      baseDir: ['dist/']
-    }
-  });
-});
-
-gulp.task('watch', function() {
-  gulp.watch('app/assets/**/*', ['styles', 'images', 'fonts'], reload);
-    gulp.watch(['app/elements/**/*'], ['vulcanize'], reload);
-    gulp.watch(['app/**/*', '!app/assets/', '!app/elements/'], ['copy'], reload);
-});
 
 gulp.task('clean', function() {
     return del.bind(null, [DEST]);
 });
 
+gulp.task('styles', function () {
+    return gulp.src([
+        'source/assets/scss/**/*.scss'
+    ])
+        .pipe($.sourcemaps.init())
+        .pipe($.sass.sync({
+            outputStyle: 'compressed',    //use 'nested' for debugging
+            precision: 10,
+        }).on('error', $.sass.logError))
+        .pipe($.autoprefixer({browsers: AUTOPREFIXER_BROWSERS}))
+        .pipe($.sourcemaps.write('./maps'))
+        .pipe(gulp.dest('source/assets/scss'))
+        .pipe($.size({
+            title: 'css'
+        }));;
+});
+
+gulp.task('patternlab', function () {
+    return gulp.src('', {read: false})
+        .pipe(shell([
+            'php core/console --generate'
+        ]))
+        .pipe(reload({stream:true}));
+});
+
+gulp.task('watch', function() {
+    gulp.watch('source/assets/**/*', ['styles'], reload);
+});
+
 gulp.task('build', function() {
-  runSequence(
-      ['clean'],
-      ['styles', 'images', 'fonts', 'vulcanize', 'copy']
-  );
+    runSequence(
+        ['clean'],
+        ['styles', 'patternlab']
+    );
 });
 
 gulp.task('default', function() {
     runSequence(
         ['build'],
-        ['watch', 'serve']
+        ['watch']
     );
 });
