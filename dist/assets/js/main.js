640b7ca02db4c09b96c410d08a4760f6da050f1d
Frederic Krossa <ymc-frkr@vcs.ymc.ch>
2016-09-20 13:56:31 +0200

    added dist forder
    SRFCMS-4529

---
+++ dist/assets/js/main.js
@@ -0,0 +1,5 @@
+import * as SrfMedia from './srf-media';
+import * as SrfCarousels from './srf-carousels';
+
+console.log(SrfMedia.init());
+console.log(SrfCarousels.init());
---
+++ dist/assets/js/srf-media.js
@@ -0,0 +1,9 @@
+export function init() {
+    return "init!";
+}
+
+export var test = "works1!! ";
+
+export function test2(){
+    console.log("function works");
+}
