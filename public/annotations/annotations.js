6337f6153aa19a62a1ad58a9d3920d2981f7c7df
Beat Christen <beat.christen@systarch.com>
2016-10-03 16:37:02 +0200

    testing with original public from starterkit

--- public/annotations/annotations.js
+++
@@ -1,1 +0,0 @@
-var comments = {"comments":[{"el":".article-header h1","title":"Article Header","comment":"<p>The article header should be no more than 140 characters. <\/p>"}]};
--- public/assets/js/main.js
+++
@@ -1,5 +0,0 @@
-import * as SrfMedia from './srf-media';
-import * as SrfCarousels from './srf-carousels';
-
-console.log(SrfMedia.init());
-console.log(SrfCarousels.init());
