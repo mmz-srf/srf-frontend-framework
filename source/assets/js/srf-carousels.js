07a935846a48c030af79945e3ead4ddfd40f227b
Roman Hatz <roman.hatz@srf.ch>
2016-10-03 17:42:23 +0200

    wokring draft
    SRFCMS-3895

--- source/assets/js/srf-carousels.js
+++ source/assets/js/srf-carousels.js
@@ -1,3 +1,12 @@
 export function init() {
-    return "init!";
+    $('.srf-carousel').slick({
+        speed: 300,
+        slidesToShow: 1,
+        prevArrow: '<a href="#" class="gallery__link--prev" />', // slick does this the wrong way around!
+        nextArrow: '<a href="#" class="gallery__link--next" />',
+        slide: ".gallery__item",
+        lazyLoad: "ondemand"
+    });
+
 }
+
