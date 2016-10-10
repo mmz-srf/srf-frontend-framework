a0194a6f8efabd87a1a7f446c8ea971aab4df74d
burimsrf <burim.muqa@srf.ch>
2016-10-10 17:24:24 +0200

    Initial commit for the video gallery
    SRFCMS-4625

--- source/assets/js/srf-carousels.js
+++ source/assets/js/srf-carousels.js
@@ -9,5 +9,38 @@ export function init() {
         lazyLoad: "ondemand"
     });
 
+    $('.video_carousel__js').slick({
+        speed: 300,
+        slidesToShow: 1,
+        infinite: false,
+        slide: ".carousel__item",
+        lazyLoad: "ondemand",
+        slidesToScroll: 1,
+        dots: false,
+        arrows: false,
+        mobileFirst: true,
+        centerMode: true,
+        responsive: [
+            {
+                breakpoint: 974, // desktop
+                settings: {
+                    slidesToShow: 3, // 10min: ~50px
+                    dots: true,
+                    arrows: true,
+                    prevArrow: '<a href="#" class="carousel__link--prev" />', // slick does this the wrong way around!
+                    nextArrow: '<a href="#" class="carousel__link--next" />'
+                }
+            }, {
+                breakpoint: 640, // tablet
+                settings: {
+                    slidesToShow: 2, // 10min: ~37px
+                    dots: true,
+                    arrows: true,
+                    prevArrow: '<a href="#" class="carousel__link--prev" />', // slick does this the wrong way around!
+                    nextArrow: '<a href="#" class="carousel__link--next" />'
+                }
+            }
+        ]
+    });
 }
 
