52cbb39e0fab57e31d2f09cd0e13133cad5d2f76
burimsrf <burim.muqa@srf.ch>
2016-10-11 16:53:30 +0200

    Styling the video gallery
    SRFCMS-4625

--- dist/assets/js/srf-carousels.js
+++ dist/assets/js/srf-carousels.js
@@ -9,5 +9,43 @@ export function init() {
         lazyLoad: "ondemand"
     });
 
+    $('.video_carousel__js').slick({
+        speed: 300,
+        infinite: false,
+        slide: ".carousel__item",
+        lazyLoad: "ondemand",
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        initialSlide: 0,
+        dots: false,
+        arrows: false,
+        mobileFirst: true,
+        centerMode: true,
+        responsive: [
+            {
+                breakpoint: 1024, // desktop
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    dots: true,
+                    arrows: true,
+                    prevArrow: '<a href="#" class="carousel__link--prev" />',
+                    nextArrow: '<a href="#" class="carousel__link--next" />',
+                    centerMode: false
+                }
+            }, {
+                breakpoint: 720, // tablet
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                    dots: true,
+                    arrows: true,
+                    prevArrow: '<a href="#" class="carousel__link--prev" />',
+                    nextArrow: '<a href="#" class="carousel__link--next" />',
+                    centerMode: false
+                }
+            }
+        ]
+    });
 }
 
--- source/assets/js/srf-carousels.js
+++ source/assets/js/srf-carousels.js
@@ -11,33 +11,38 @@ export function init() {
 
     $('.video_carousel__js').slick({
         speed: 300,
-        slidesToShow: 1,
         infinite: false,
         slide: ".carousel__item",
         lazyLoad: "ondemand",
+        slidesToShow: 1,
         slidesToScroll: 1,
+        initialSlide: 0,
         dots: false,
         arrows: false,
         mobileFirst: true,
         centerMode: true,
         responsive: [
             {
-                breakpoint: 974, // desktop
+                breakpoint: 1024, // desktop
                 settings: {
-                    slidesToShow: 3, // 10min: ~50px
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
                     dots: true,
                     arrows: true,
-                    prevArrow: '<a href="#" class="carousel__link--prev" />', // slick does this the wrong way around!
-                    nextArrow: '<a href="#" class="carousel__link--next" />'
+                    prevArrow: '<a href="#" class="carousel__link--prev" />',
+                    nextArrow: '<a href="#" class="carousel__link--next" />',
+                    centerMode: false
                 }
             }, {
-                breakpoint: 640, // tablet
+                breakpoint: 720, // tablet
                 settings: {
-                    slidesToShow: 2, // 10min: ~37px
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
                     dots: true,
                     arrows: true,
-                    prevArrow: '<a href="#" class="carousel__link--prev" />', // slick does this the wrong way around!
-                    nextArrow: '<a href="#" class="carousel__link--next" />'
+                    prevArrow: '<a href="#" class="carousel__link--prev" />',
+                    nextArrow: '<a href="#" class="carousel__link--next" />',
+                    centerMode: false
                 }
             }
         ]
