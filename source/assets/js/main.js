a24f89761003caf333e52c76af8849f74898dbde
nobelre <regula.nobel@srf.ch>
2016-11-01 18:23:37 +0100

    setup diagrams
    SRFCMS-4626

--- source/assets/js/main.js
+++ source/assets/js/main.js
@@ -1,5 +1,7 @@
 import {init as SrfCarouselsInit} from './srf-carousels';
+import {init as SrfDiagramInit} from './srf-diagram';
 
 document.addEventListener("DOMContentLoaded", function(event) {
     SrfCarouselsInit();
+    SrfDiagramInit();
 });
---
+++ source/assets/js/srf-diagram.js
@@ -0,0 +1,32 @@
+export function init() {
+    AmCharts.ready(function () {
+        console.log(123)
+        $('.chart').each(function () {
+            loadChart($(this));
+        });
+
+        function loadChart($element) {
+            var url = $element.data('src');
+
+            $.getJSON(url, function (settings) {
+
+                if (settings['graphs']) {
+                    if (settings['graphs'].length >= 5) {
+                        $element.addClass('mobile').addClass('line');
+                    }
+                }
+                if (settings['type'] === 'pie') {
+                    if (settings['dataProvider'].length >= 7) {
+                        $element.addClass('mobile').addClass('pie');
+                    }
+                }
+
+                var chart = AmCharts.makeChart($element.attr('id'), settings);
+                /* if (chart.legend) {
+                 chart.legend.addListener("showItem", updateTextLength);
+                 chart.legend.addListener("hideItem", updateTextLength);
+                 } */
+            });
+        }
+    });
+}
