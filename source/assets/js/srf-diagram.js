759b6b465e16e9e917ee4782c9b37209bc5c6bf3
nobelre <regula.nobel@srf.ch>
2016-11-03 17:15:44 +0100

    uglifying the js - but solving an amcharts bug: OPSUP-4953 (still unresolved by v.3)
    SRFCMS-4626

--- source/assets/js/srf-diagram.js
+++ source/assets/js/srf-diagram.js
@@ -1,31 +1,62 @@
 export function init() {
     AmCharts.ready(function () {
+
         $('.chart').each(function () {
             loadChart($(this));
         });
 
         function loadChart($element) {
+
             var url = $element.data('src');
 
             $.getJSON(url, function (settings) {
 
                 if (settings['graphs']) {
                     if (settings['graphs'].length >= 5) {
-                        $element.addClass('mobile').addClass('line');
+                        $element.addClass('line');
                     }
                 }
                 if (settings['type'] === 'pie') {
                     if (settings['dataProvider'].length >= 7) {
-                        $element.addClass('mobile').addClass('pie');
+                        $element.addClass('pie');
                     }
                 }
 
                 var chart = AmCharts.makeChart($element.attr('id'), settings);
-                /* if (chart.legend) {
-                 chart.legend.addListener("showItem", updateTextLength);
-                 chart.legend.addListener("hideItem", updateTextLength);
-                 } */
+                chart.addListener("drawn", updateTextLength);
             });
+        }
+    });
+
+    function updateTextLength() {
+        var boxWidth = 0;
+        $(".chart").each(function(i) {
+            var legend = $(this).find(".amcharts-legend-bg");
+            if (legend.length) {
+                boxWidth = parseInt(legend[0].getBoundingClientRect().width, 10) - 24;
+                var string = "";
+                $(this).find(".amcharts-legend-label").each(function(j) {
+                    string = $(this).find("tspan");
+                    if ($(this)[0].getBoundingClientRect().width >= boxWidth) {
+                        var k = 6;
+                        var end = parseInt((boxWidth / k), 10);
+                        if (string.text().length <= end) {
+                            k = 7;
+                            end = parseInt((boxWidth / k), 10);
+                        }
+                        string.text(string.text().substring(0, end) + " ...");
+                    }
+                });
+            }
+        });
+    }
+
+    //* there's no init event :( *//
+    $( document ).ready(function() {
+        if ($(".chart").length) {
+            setTimeout(function () {
+                updateTextLength();
+            }, 500);
         }
     });
 }
