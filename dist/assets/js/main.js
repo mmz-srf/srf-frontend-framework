16758c30590cf38dae75836ad5603f7f5d73eac4
Beat Christen <beat.christen@systarch.com>
2017-01-26 12:44:55 +0100

    thou shalt not commit build artifacts
    SRFCMS-5580

--- dist/assets/js/main.js
+++
@@ -1,7 +0,0 @@
-import {init as SrfCarouselsInit} from './srf-carousels';
-import {init as SrfChartsInit} from './srf-charts';
-
-document.addEventListener("DOMContentLoaded", function(event) {
-    SrfCarouselsInit();
-    SrfChartsInit();
-});
