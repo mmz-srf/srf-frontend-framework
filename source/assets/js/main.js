b87a1c28e3bcbd20579a87b169e47bc84745f074
Hasan Kryeziu <hasan.kryeziu@srf.ch>
2017-01-30 11:05:30 +0100

    implement image slider
    SRFCMS-4642

--- source/assets/js/main.js
+++ source/assets/js/main.js
@@ -1,7 +1,10 @@
 import {init as SrfCarouselsInit} from './srf-carousels';
 import {init as SrfChartsInit} from './srf-charts';
+import {init as SrfImageSliderInit} from './srf-image-slider';
+
 
 document.addEventListener("DOMContentLoaded", function(event) {
     SrfCarouselsInit();
     SrfChartsInit();
+    SrfImageSliderInit();
 });
