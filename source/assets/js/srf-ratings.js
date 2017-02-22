b2f5d6bc389b582e5bf30db6de776bc4039d905f
Stefan Rechsteiner <info@stefanrechsteiner.com>
2017-02-22 16:03:54 +0100

    Cleans up CSS and JS and adds comments to JS
    SRFCMS-5261

--- source/assets/js/srf-ratings.js
+++ source/assets/js/srf-ratings.js
@@ -1,41 +1,39 @@
 (function ($) {
     "use strict";
 
+    // set global variables
     var getTotalVotes,
         getTotalStars,
         resultVote = 0,
         totalVotes = 0,
         totalStars = 0;
 
+    // process data to get total of votes
     function getTotalVotes(data){
         for (var i = 0; i < data.length; i++) { totalVotes += data[i]; }
         return totalVotes;
     };
-        
+
+    // process data to get total stars
     function getTotalStars(data){
-        console.log('Votes: '+data);
-
-        $.each(data, function(i,v){
-            totalStars += v*(i+1);
-        });
-        console.log('totalStars: '+totalStars);
-
+        for (var i = 0; i < data.length; i++) { totalStars += data[i] * ( i + 1 ); }
         return totalStars;
     };
 
     $(document).ready(function () {
 
+        // load ratings-results and process data
         var dataJSONurl = $('.devWrap').attr('data-src');
-
         $.getJSON(dataJSONurl, function(data) {
             getTotalVotes(data);
             getTotalStars(data);
         });
 
+        // if user votes
         $('.ratingstars--toVote input[type="radio"]').on('change',function(e){
             var $that = $(this),
                 myVote = $that.attr('value'),
-                $ratingstarsContainer = $('.ratingstars'),
+                $ratingstarsContainer = $('.devWrap'),
                 animeStarName = '@keyframes ratingAnime',
                 animeStarCode = '{0% {transform:translate3d(0,0,0) scale3d(1,1,1)} 50% {transform:translate3d(0,-24px,0) scale3d(1.125,1.125,1.125) rotateY(90deg)} 80% {transform:translate3d(0,-19.2px,0) scale3d(.66,.66,.66) rotateY(144deg)} 100% {transform:translate3d(0,-12px,0) scale3d(.875,.875,.875) rotateY(180deg);',
                 animeStarOrigin = '',
@@ -48,14 +46,18 @@
                 animeStarNeutral = 'fill:rgb(185,183,173)}}',
                 newStyle = '';
 
+            // calculate the rating-result
             resultVote = Math.round((totalStars + myVote) / (totalVotes + 1)) / 10;
-            
+
+            // mark all stars before (and including) the clicked one as active
             for (var i = 1; i <= myVote; i++) {
                 $('.ratingstars__star--'+i).addClass('is-active');
             }
 
+            // compose the keyframes
             for (var i = 1; i < 6; i++) {
 
+                // set the transform-origin to the appropriate value for each star
                 if (i == 1) {
                     animeStarOrigin = animeStarOrigin1;
                 } else if (i == 2) {
@@ -68,6 +70,7 @@
                     animeStarOrigin = animeStarOrigin5;
                 }
 
+                // compose the css-keyframe-code for all five individual stars … active or neutral
                 if (i <= Math.round(resultVote)) {
                     newStyle += animeStarName+i+' '+animeStarCode+animeStarOrigin+animeStarActive;
                 } else {
@@ -76,12 +79,15 @@
 
             }
 
+            // add the keyframe-codes into a style-element and add it to the DOM
             $('<style />').text(newStyle).insertAfter($ratingstarsContainer);
 
+            // include Result- and myVote-Number to the DOM
             $('.resultNumber--js').text(resultVote);
             $('.myNumber--js').text(myVote);
+
+            // add css-classes to trigger the animations
             $ratingstarsContainer.addClass('animated');
-            $('.resultWording').addClass('animated');
 
         });
 
