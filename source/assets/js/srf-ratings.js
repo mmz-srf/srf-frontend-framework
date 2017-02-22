cb10097bdc8ea97da679844ae2d5305c133087f8
Stefan Rechsteiner <info@stefanrechsteiner.com>
2017-02-22 15:36:04 +0100

    ⭐ freaking-complicated but equally-amazing ratings animation
    SRFCMS-5261

--- source/assets/js/srf-ratings.js
+++ source/assets/js/srf-ratings.js
@@ -1,49 +1,87 @@
 (function ($) {
     "use strict";
 
-    var getResult,
-        resultVote = 0;
+    var getTotalVotes,
+        getTotalStars,
+        resultVote = 0,
+        totalVotes = 0,
+        totalStars = 0;
 
-    function getResult(data){
+    function getTotalVotes(data){
+        for (var i = 0; i < data.length; i++) { totalVotes += data[i]; }
+        return totalVotes;
+    };
+        
+    function getTotalStars(data){
         console.log('Votes: '+data);
-        
-        var totalVotes = 0,
-            totalStars = 0;
 
-        for (var i = 0; i < data.length; i++) {
-            totalVotes += data[i];
-        }
-        console.log('totalVotes: '+totalVotes);
-        
         $.each(data, function(i,v){
             totalStars += v*(i+1);
         });
         console.log('totalStars: '+totalStars);
 
-        resultVote = Math.round( (totalStars / totalVotes) * 10 ) / 10;
-        console.log('resultVote: '+resultVote)
-
-        $('.resultNumber--js').text(resultVote);
-
+        return totalStars;
     };
 
     $(document).ready(function () {
 
-        console.log('loaded');
-        
         var dataJSONurl = $('.devWrap').attr('data-src');
-        
+
         $.getJSON(dataJSONurl, function(data) {
-            getResult(data);
+            getTotalVotes(data);
+            getTotalStars(data);
         });
 
         $('.ratingstars--toVote input[type="radio"]').on('change',function(e){
             var $that = $(this),
                 myVote = $that.attr('value'),
-                $ratingstarsContainer = $('.ratingstars');
+                $ratingstarsContainer = $('.ratingstars'),
+                animeStarName = '@keyframes ratingAnime',
+                animeStarCode = '{0% {transform:translate3d(0,0,0) scale3d(1,1,1)} 50% {transform:translate3d(0,-24px,0) scale3d(1.125,1.125,1.125) rotateY(90deg)} 80% {transform:translate3d(0,-19.2px,0) scale3d(.66,.66,.66) rotateY(144deg)} 100% {transform:translate3d(0,-12px,0) scale3d(.875,.875,.875) rotateY(180deg);',
+                animeStarOrigin = '',
+                animeStarOrigin1 = 'transform-origin:80% 50%;',
+                animeStarOrigin2 = 'transform-origin:65% 50%;',
+                animeStarOrigin3 = 'transform-origin:50% 50%;',
+                animeStarOrigin4 = 'transform-origin:35% 50%;',
+                animeStarOrigin5 = 'transform-origin:20% 50%;',
+                animeStarActive = 'fill:rgb(34,33,29)}}',
+                animeStarNeutral = 'fill:rgb(185,183,173)}}',
+                newStyle = '';
 
-            $('<style>@keyframes ratingAnime1 {0% {transform:translate3d(0,0,0) scale3d(1,1,1)}    50% {transform:translate3d(0,-32px,0) scale3d(1.125,1.125,1.125) rotateY(90deg)}    80% {transform:translate3d(0,-10px,0) scale3d(.8,.8,.8) rotateY(144deg)}    100% {transform:translate3d(0,-16px,0) scale3d(.875,.875,.875) rotateY(180deg);transform-origin:80% 50%;fill:rgb(34,33,29)}}@keyframes ratingAnime2 {    0% {transform:translate3d(0,0,0) scale3d(1,1,1)}    50% {transform:translate3d(0,-32px,0) scale3d(1.125,1.125,1.125) rotateY(90deg)}    80% {transform:translate3d(0,-10px,0) scale3d(.8,.8,.8) rotateY(144deg)}    100% {transform:translate3d(0,-16px,0) scale3d(.875,.875,.875) rotateY(180deg);transform-origin:65% 50%;fill:rgb(34,33,29)}}@keyframes ratingAnime3 {    0% {transform:translate3d(0,0,0) scale3d(1,1,1)}    50% {transform:translate3d(0,-32px,0) scale3d(1.125,1.125,1.125) rotateY(90deg)}    80% {transform:translate3d(0,-10px,0) scale3d(.8,.8,.8) rotateY(144deg)}    100% {transform:translate3d(0,-16px,0) scale3d(.875,.875,.875) rotateY(180deg);transform-origin:50% 50%;fill:rgb(34,33,29)}}@keyframes ratingAnime4 {    0% {transform:translate3d(0,0,0) scale3d(1,1,1)}    50% {transform:translate3d(0,-32px,0) scale3d(1.125,1.125,1.125) rotateY(90deg)}    80% {transform:translate3d(0,-10px,0) scale3d(.8,.8,.8) rotateY(144deg)}    100% {transform:translate3d(0,-16px,0) scale3d(.875,.875,.875) rotateY(180deg);transform-origin:35% 50%;fill:rgb(185,183,173)}}@keyframes ratingAnime5 {    0% {transform:translate3d(0,0,0) scale3d(1,1,1)}    50% {transform:translate3d(0,-32px,0) scale3d(1.125,1.125,1.125) rotateY(90deg)}    80% {transform:translate3d(0,-10px,0) scale3d(.8,.8,.8) rotateY(144deg)}    100% {transform:translate3d(0,-16px,0) scale3d(.875,.875,.875) rotateY(180deg);transform-origin:20% 50%;fill:rgb(185,183,173)}}</style>').insertAfter($ratingstarsContainer);
+            resultVote = Math.round((totalStars + myVote) / (totalVotes + 1)) / 10;
+            
+            for (var i = 1; i <= myVote; i++) {
+                $('.ratingstars__star--'+i).addClass('is-active');
+            }
+
+            for (var i = 1; i < 6; i++) {
+
+                if (i == 1) {
+                    animeStarOrigin = animeStarOrigin1;
+                } else if (i == 2) {
+                    animeStarOrigin = animeStarOrigin2;
+                } else if (i == 3) {
+                    animeStarOrigin = animeStarOrigin3;
+                } else if (i == 4) {
+                    animeStarOrigin = animeStarOrigin4;
+                } else if (i == 5) {
+                    animeStarOrigin = animeStarOrigin5;
+                }
+
+                if (i <= Math.round(resultVote)) {
+                    newStyle += animeStarName+i+' '+animeStarCode+animeStarOrigin+animeStarActive;
+                } else {
+                    newStyle += animeStarName+i+' '+animeStarCode+animeStarOrigin+animeStarNeutral;
+                }
+
+            }
+
+            $('<style />').text(newStyle).insertAfter($ratingstarsContainer);
+
+            $('.resultNumber--js').text(resultVote);
+            $('.myNumber--js').text(myVote);
             $ratingstarsContainer.addClass('animated');
+            $('.resultWording').addClass('animated');
 
         });
 
