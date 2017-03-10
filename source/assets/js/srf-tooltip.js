4ff5d20d4499c15bec9f609ea43d79a354d39c2d
Alexej Bondarenko <alexej.bondarenko@ersocon.net>
2017-03-10 11:32:07 +0100

    Disable tooltips on touch devices

--- source/assets/js/srf-tooltip.js
+++ source/assets/js/srf-tooltip.js
@@ -2,19 +2,41 @@ export function init() {
 
     $(document).ready(function() {
 
+        var clientTouchSupported = 'ontouchstart' in window;
+
         // Bind mouseenter and mouseleave handlers
         $('[data-tooltip-toggle]').each(function() {
 
             var $that = $(this);
 
             $that.title = $that.data('tooltipContent');
+
+            // Set correct value for touch enabled flag (YAGNI?)
+            $that.touchEnabled = (typeof $that.data('tooltipTouch')) !== 'undefined';
+
+            $that.tooltipEnabled = false;
+
+            // Explicit check for provided data-tooltip-content attribute
+            if (typeof $that.title !== 'undefined') {
+                $that.tooltipEnabled = true;
+            } else {
+                console.warn("Tooltip used without content, please add data-tooltip-content to enable tooltip");
+            }
+
+            // We need the original size before insertion of tooltip content
             $that.originalWidth = $that.width();
 
             $that.template = '<div class="tooltip" role="tooltip"><div class="tooltip-arrow"></div>'
                            + '<div class="tooltip-content"></div></div>';
 
-            // Only bind if title is set
-            if (typeof $that.title !== 'undefined') {
+
+            // Disable on touch devices if not explicitly set
+            if (clientTouchSupported && !$that.touchEnabled) {
+                $that.tooltipEnabled = false;
+            }
+
+            // Only bind if tooltip is enabled
+            if ($that.tooltipEnabled) {
                 $that.mouseenter(function() {
                     $that.append($that.template);
                     $that.css('position', 'relative');
@@ -37,8 +59,6 @@ export function init() {
                 $that.mouseleave(function() {
                     $that.children('.tooltip').remove();
                 });
-            } else {
-                console.warn("Tooltip used without content, please add data-tooltip-content");
             }
         });
     });
