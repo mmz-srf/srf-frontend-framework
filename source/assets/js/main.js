d82e7e045eac051bde6aa1bc11a9048db1bef482
nobelre <regula.nobel@srf.ch>
2017-03-20 08:28:38 +0100

    showing all in a form
    SRFCMS-6150

--- source/assets/js/main.js
+++ source/assets/js/main.js
@@ -4,6 +4,7 @@ import {init as SrfCHMapInit} from './srf-chmap';
 import {init as SrfImageSliderInit} from './srf-image-slider';
 import {init as SrfPollInit} from './srf-poll';
 import {init as SrfTooltipInit} from './srf-tooltip';
+import {init as SrfFormFieldInit} from './srf-form-fields';
 
 
 document.addEventListener("DOMContentLoaded", function(event) {
@@ -13,4 +14,5 @@ document.addEventListener("DOMContentLoaded", function(event) {
     SrfImageSliderInit();
     SrfPollInit();
     SrfTooltipInit();
+    SrfFormFieldInit();
 });
