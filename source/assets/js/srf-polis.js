9ace6fb6da1b6a465d1b5567db94131393e93195
nobelre <regula.nobel@srf.ch>
2017-03-27 17:26:20 +0200

    removing a typo from js
    SRFCMS-4639

--- source/assets/js/srf-polis.js
+++ source/assets/js/srf-polis.js
@@ -482,12 +482,10 @@ var polisController = function() {
 
         this.generateMobileNavigation = function() {
             var $select = $('#' + this.id).closest('.polis-container').find('.menu');
-            // console.log($select.find("option"))
-            // $select.find("option").addClass("polis-select-option--hide");
+            $select.find("option").addClass("polis-select-option--hide");
             for (var cantonId in this.cantons) {
-                console.log(this.cantons[cantonId]);
                 if (this.cantons[cantonId].hasResults()) {
-                    $select.find("option[value" + cantonId + "]").removeClass("polis-select-option--hide");
+                    $select.find("option[value=" + cantonId + "]").removeClass("polis-select-option--hide");
                 }
             }
         };
