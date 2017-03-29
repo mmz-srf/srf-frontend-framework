5bb1dffd6cfca727cce6ec7cedc0258a175ea046
nobelre <regula.nobel@srf.ch>
2017-03-29 19:18:11 +0200

    make it even possible to uncheck an option ...
    SRFCMS-6367

--- source/assets/js/srf-form-fields.js
+++ source/assets/js/srf-form-fields.js
@@ -3,7 +3,8 @@ export function init() {
     $(".radio-button, .checkbox").on("keypress", function (e) {
         // enable checking radios by tabbing in + <enter>
         if (e.keyCode === 13) {
-            $(this).prop('checked', true);
+            var isChecked = $(this).prop('checked') ? false : true;
+            $(this).prop('checked', isChecked);
             return false;
         }
     });
