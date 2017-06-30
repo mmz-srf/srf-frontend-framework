dad19479bc46e43d66a3356cfce52af4232e8966
nobelre <regula.nobel@srf.ch>
2017-06-30 20:27:09 +0200

    fancy bg and radio navi as well ...
    SRFCMS-6060

--- source/assets/js/srf-header.js
+++ source/assets/js/srf-header.js
@@ -3,12 +3,25 @@ export function init() {
         e.preventDefault();
         let $handle = $(this);
         if ($handle.hasClass("menu-handle--active")) {
-            $(".l-main-wrapper").removeClass("js-wrapper-observer")// .find(".navbar").addClass("navbar--go-away");
+            $(".l-main-wrapper").removeClass("js-wrapper-observer");
             $handle.removeClass("menu-handle--active");
-            $(".navbar__menu").removeClass("navbar__menu--come-in").one("transitionend", function () {
-                $(this).closest(".navbar").addClass("navbar--closed")// .removeClass("navbar--go-away")
+
+            if (!$(window).width() > 719) { // there are animations we have to wait for....
+                $(".navbar__menu").removeClass("navbar__menu--come-in").one("transitionend", function () {
+                    $(this).closest(".navbar").addClass("navbar--closed")
+                        .closest(".l-main-wrapper").removeClass("wrapper--fixed");
+                });
+            } else {
+                /* $handle.closest(".l-main-wrapper").removeClass("wrapper--fixed")
+                 .find(".navbar").one("transitionend", function () {
+                 // $(".navbar__menu").removeClass("navbar__menu--come-in").one("transitionend", function () {
+                 $(this).addClass("navbar--closed").find(".navbar__menu").removeClass("navbar__menu--come-in")
+                 // .closest(".l-main-wrapper").removeClass("wrapper--fixed");
+                 }); */ // <-- this looks shite!
+                $(".navbar__menu").removeClass("navbar__menu--come-in")
+                    .closest(".navbar").addClass("navbar--closed")
                     .closest(".l-main-wrapper").removeClass("wrapper--fixed");
-            });
+            }
 
         } else {
             e.stopPropagation();
@@ -20,9 +33,10 @@ export function init() {
     });
 
     $(document).on("click", ".js-wrapper-observer", function (e) {
-        $target = $(e.target);
+        let $target = $(e.target);
+        console.log($target)
         // make it possible to use search while page is dimmed and navi is visible
-        if (!$target.hasClass("searchbox__input") && !$target.hasClass("navbar__link")) {
+        if (!$target.hasClass("searchbox__input") && !$target.hasClass("navbar__link") && !$target.hasClass("expand-arrow")) {
             $(".menu-handle").trigger("click");
         }
     });
@@ -32,10 +46,10 @@ export function init() {
         let $handle = $(this), $arrow = $handle.find(".expand-arrow");
         if ($arrow.hasClass("expand-arrow--open")) { // radio menu is open
             $arrow.removeClass("expand-arrow--open");
-            $handle.next(".navbar__group--radio").addClass("h-element--hide");
+            $handle.next(".navbar__group--radio").removeClass("navbar__group--radio-open"); // .addClass("h-element--hide");
         } else { // it's closed
             $arrow.addClass("expand-arrow--open");
-            $handle.next(".navbar__group--radio").removeClass("h-element--hide");
+            $handle.next(".navbar__group--radio").addClass("navbar__group--radio-open"); // .removeClass("h-element--hide");
         }
     });
 }
