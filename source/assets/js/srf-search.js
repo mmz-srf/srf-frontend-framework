6430b1f06519da9e2463fd5e128c490b58ad2842
nobelre <regula.nobel@srf.ch>
2017-07-25 17:25:16 +0200

    "completing" search (hopefully)
    SRFCMS-6249

--- source/assets/js/srf-search.js
+++ source/assets/js/srf-search.js
@@ -8,16 +8,16 @@ function registerListener() {
         let $input = $(this);
         let $button = $input.closest(".searchbox").find("button");
         if ($input.val().length > 2 && $button.attr("tabindex") == -1) {
-            $button.attr("tabindex", 0);
+            $button.attr("tabindex", 0).attr("aria-hidden", false);
         } else if ($input.val().length == 0 && $button.attr("tabindex") == 0) {
-            $button.attr("tabindex", -1);
+            $button.attr("tabindex", -1).attr("aria-hidden", true);
         }
     });
 
     $(".searchbox").on("submit", function () {
         let $input = $(this).find(".searchbox__input");
         $input.val("");
-        $input.closest(".searchbox").find("button").attr("tabindex", -1);
+        $input.closest(".searchbox").find("button").attr("tabindex", -1).attr("aria-hidden", true);
     });
 }
 
