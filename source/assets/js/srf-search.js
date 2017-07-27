2ac81f672ee98619d0f614c71c5458e789d28074
nobelre <regula.nobel@srf.ch>
2017-07-27 19:12:19 +0200

    so we disable it for mobiles :)
    SRFCMS-6249

--- source/assets/js/srf-search.js
+++ source/assets/js/srf-search.js
@@ -6,13 +6,15 @@ function registerListener() {
 
     // we enable the <submit> button "as soon" as search text ist entered
     $(".searchbox").on("keyup", ".searchbox__input", function (e) {
-        let $input = $(this);
-        let $button = $input.closest(".searchbox").find("button");
-        // this works mobile as well unlike <enter>-keys an d the likes
-        if ($input.val().length > 2 && $button.attr("tabindex") == -1) {
-            $button.attr("tabindex", 0).attr("aria-hidden", false);
-        } else if ($input.val().length == 0 && $button.attr("tabindex") == 0) {
-            $button.attr("tabindex", -1).attr("aria-hidden", true);
+        if ($(window).width() > 719) { // a search button only makes sense on desktop - when it's actually working
+            let $input = $(this);
+            let $button = $input.closest(".searchbox").find("button");
+            // this works mobile as well unlike <enter>-keys an d the likes
+            if ($input.val().length > 2 && $button.attr("tabindex") == -1) {
+                $button.attr("tabindex", 0).attr("aria-hidden", false);
+            } else if ($input.val().length == 0 && $button.attr("tabindex") == 0) {
+                $button.attr("tabindex", -1).attr("aria-hidden", true);
+            }
         }
     });
 }
