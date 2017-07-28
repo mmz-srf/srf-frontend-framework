fba28c9161a5f12b74fa818aea91133fb65152e2
Philipp Christen <philipp.christen@srf.ch>
2017-07-28 15:21:26 +0200

    further refactoring, focus fixing

--- source/assets/js/srf-header.js
+++ source/assets/js/srf-header.js
@@ -1,89 +1,36 @@
 export function init() {
 
+    let MENU_EVENT = "srf_handle-menu";
     let HANDLE_CLASS = ".menu-handle";
-    let ARROW_CLASS = ".expand-arrow";
-
+    let SUBMENU_CLASS = ".js-expand-arrow";
+    let DESKTOP_CLOSE_BTN_CLASS = ".navbar__link--close";
     let KEYCODES = {
         "enter": 13,
         "tab": 9
     };
+    let WIN_SIZE_DESKTOP = 719;
 
     let $header = $(".header");
     let $handle = $(HANDLE_CLASS);
-    let $arrow = $handle.find(ARROW_CLASS);
+    let $arrow = $(".expand-arrow");
+    let $info = $(".js-expand-info");
+    let $subMenuHeader = $(SUBMENU_CLASS);
+    let $subMenuContent = $(".navbar__group--radio");
+    let $desktopCloseBtn = $(DESKTOP_CLOSE_BTN_CLASS);
 
     let menuHasFocus = false;
 
-    $header.on("keydown", HANDLE_CLASS, (e) => handleKeyPress)
-           .on("keydown", ".navbar__link--close", (e) => handleKeyPress)
-           .on("click srf_handle-menu", HANDLE_CLASS, (e) => onMenuHandling);
+    $header.on("keydown", HANDLE_CLASS, (e) => handleKeyPress(e) )
+        .on("keydown", DESKTOP_CLOSE_BTN_CLASS, (e) => handleKeyPress(e) )
+        .on("click MENU_EVENT", HANDLE_CLASS, (e) => onMenuHandling(e) );
 
-    $(document).on("click touchstart", ".body--observer", (e) => handleClick);
+    $(document).on("click touchstart", ".body--observer", (e) => handleBodyClick(e) );
 
     // accessibility: if menu loses focus => we close it
-    $(".breadcrumbs").on("keyup", (e) => handleBreadcrumbsFocus);
+    $(".breadcrumbs").on("keyup", (e) => handleBreadcrumbsFocus(e) );
 
     // radiostation navigation opening & closing
-    $(".navbar__menu").on("click", ".js-expand-arrow", (e) => handleExpandArrowClick);
-    
-    // no touch highlight - but css :active-styles for links
-    document.addEventListener("touchstart", () => {}, false);
-
-    function closeMenu() {
-        $("body").removeClass("body--observer")
-                 .find(".navbar__link--close")
-                 .removeClass("navbar__link--fixed");
-
-        $handle.removeClass("menu-handle--active");
-
-        if ($(window).width() > 719) { // there are animations we have to wait for....
-
-            $(".navbar__menu").removeClass("navbar__menu--come-in").one("transitionend", () => {
-
-                $(this).closest(".navbar").addClass("navbar--closed")
-                       .closest("body").removeClass("body--fixed");
-
-                if ($arrow.hasClass("expand-arrow--open")) {
-                    $(".js-expand-arrow").trigger("click");
-                }
-            });
-
-        } else { // on mobile: no animations
-
-            $(".navbar__menu").removeClass("navbar__menu--come-in") // slide menu back
-                              .closest(".navbar").addClass("navbar--closed")
-                              .closest("body").removeClass("body--fixed");
-
-            if ($arrow.hasClass("expand-arrow--open")) {
-                $(".js-expand-arrow").trigger("click");
-            }
-        }
-        menuHasFocus = false;
-    }
-
-    function openMenu() {
-        $handle.addClass("menu-handle--active")
-               .closest("body").addClass("body--fixed").addClass("body--observer")
-               .find(".navbar").removeClass("navbar--closed")
-               .find(".navbar__menu").addClass("navbar__menu--come-in");
-
-        if ($(window).width() > 719) { // there are animations we have to wait for....
-            $(".navbar__menu").one("transitionend", () => {
-                $(this).find(".navbar__link--close").addClass("navbar__link--fixed");
-            });
-        }
-        menuHasFocus = true;
-    }
-
-    function handleKeyPress(e) {
-        if (e.keyCode === KEYCODES.enter) {
-            // focus on [menu]
-            $handle.trigger("srf_handle-menu");
-            $handle.focus();
-
-            return false;
-        }
-    }
+    $(".navbar__menu").on("click", SUBMENU_CLASS, (e) => handleExpandArrowClick(e) );
 
     function onMenuHandling(e) {
         e.preventDefault(); // chrome has a problem (bug!) with keypress!
@@ -92,49 +39,104 @@ export function init() {
         if (menuHasFocus) {
             closeMenu(e);
         } else {
-            openMenu();
+            openMenu(e);
+        }
+    }
 
-            if (e.type === "srf_handle-menu") {
-                $(".navbar__link--close").focus();
+    function closeMenu(e) {
+        $("body").removeClass("body--observer");
+
+        $handle.removeClass("menu-handle--active");
+
+        let finishClosingMenu = () => {
+            $(".navbar__menu").closest(".navbar").addClass("navbar--closed")
+                .closest("body").removeClass("body--fixed");
+
+            if ($arrow.hasClass("expand-arrow--open")) {
+                handleExpandArrowClick();
             }
+
+            if (e && e.type === "keydown") {
+                $handle.focus();
+            }
+        };
+
+        // Desktop: there are animations we have to wait for
+        if (isDesktop()) {
+            $(".navbar__menu").removeClass("navbar__menu--come-in").one("transitionend", () => {
+                finishClosingMenu();
+                $desktopCloseBtn.removeClass("navbar__link--fixed");
+            });
+        } else {
+            $(".navbar__menu").removeClass("navbar__menu--come-in");
+            finishClosingMenu();
+        }
+
+        menuHasFocus = false;
+    }
+
+    function openMenu(e) {
+        $handle.addClass("menu-handle--active")
+            .closest("body").addClass("body--fixed body--observer")
+            .find(".navbar").removeClass("navbar--closed")
+            .find(".navbar__menu").addClass("navbar__menu--come-in");
+
+        if (isDesktop()) {
+            $(".navbar__menu").one("transitionend", () => {
+                $desktopCloseBtn.addClass("navbar__link--fixed");
+
+                if (e && e.type === "keydown") {
+                    $desktopCloseBtn.focus();
+                }
+            });
+        }
+
+        menuHasFocus = true;
+    }
+
+    function handleKeyPress(e) {
+        if (e.keyCode === KEYCODES.enter) {
+            onMenuHandling(e);
         }
     }
 
     function handleExpandArrowClick(e) {
-        e.preventDefault();
+        typeof e !== "undefined" ? e.preventDefault() : null;
 
-        let $info = $(".js-expand-info");
+        let subMenuIsOpen = $arrow.hasClass("expand-arrow--open");
 
-        if ($arrow.hasClass("expand-arrow--open")) { // radio menu is open => close it
+        $arrow.toggleClass("expand-arrow--open", !subMenuIsOpen);
+        $subMenuHeader.attr("aria-expanded", !subMenuIsOpen);
+        $subMenuContent.toggleClass("navbar__group--radio-open", !subMenuIsOpen);
 
-            $arrow.removeClass("expand-arrow--open");
-            $handle.attr("aria-expanded", false).next(".navbar__group--radio")
-                   .removeClass("navbar__group--radio-open");
-
+        if (subMenuIsOpen) {
             $info.text($info.data("text-open"));
-        } else { // it's closed => open it
-            $arrow.addClass("expand-arrow--open");
-
-            $handle.attr("aria-expanded", true).next(".navbar__group--radio")
-                   .addClass("navbar__group--radio-open");
-
+        } else {
             $info.text($info.data("text-close"));
         }
     }
 
     function handleBreadcrumbsFocus(e) {
-        // we tabbed "into article"
         if (menuHasFocus && e.keyCode === KEYCODES.tab) {
-            $handle.trigger("click");
+            closeMenu();
         }
     }
 
-    function handleClick(e) {
+    function handleBodyClick(e) {
         let $target = $(e.target);
 
         // make it possible to use search while page is dimmed and navi is visible
-        if (!$target.hasClass("searchbox__input") && (!$target.hasClass("navbar__link") || $target.hasClass("navbar__link--close") ) && !$target.hasClass("expand-arrow")) {
-            $handle.trigger("click");
+        if (!$target.hasClass("searchbox__input")
+            && (!$target.hasClass("navbar__link") || $target.hasClass("navbar__link--close") )
+            && !$target.hasClass("expand-arrow")
+            && !$target.hasClass("menu-handle")
+            && !$target.hasClass("menu-handle__info") ) {
+
+            onMenuHandling(e);
         }
+    }
+
+    function isDesktop() {
+        return $(window).width() > WIN_SIZE_DESKTOP;
     }
 }
