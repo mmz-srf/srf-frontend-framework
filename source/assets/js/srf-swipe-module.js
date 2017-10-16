fc2849b17e7c583f621510eabd9545cab02a63f7
Philipp Christen <philipp.christen@srf.ch>
2017-10-16 09:28:42 +0200

    button logic on swipeModule
    SRFCMSAL-326

--- source/assets/js/srf-swipe-module.js
+++ source/assets/js/srf-swipe-module.js
@@ -5,6 +5,8 @@ export function init() {
 }
 
 const animationSpeed = 500;
+const DIRECTION = {LEFT: "left", RIGHT: "right"};
+const BREAKPOINT_2 = 768;
 
 export class SrfSwiper {
 
@@ -12,25 +14,30 @@ export class SrfSwiper {
         this.$element = $(element);
         this.$swipeContainer = this.$element.find(".swipemod-swipecontainer");
         this.$container = this.$element.find(".swipemod-container");
-        this.$containerPadding = parseInt(this.$container.css("paddingLeft"));
+        this.containerPadding = parseInt(this.$container.css("paddingLeft"));
+
+        this.$items = this.$element.find(".swipemod-item");
+        this.$prevBtn = this.$element.find(".swipemod-button[data-direction='left']");
+        this.$nextBtn = this.$element.find(".swipemod-button[data-direction='right']");
 
         this.registerListeners();
+
+        this.showHidePrevNextButtons();
     }
 
     registerListeners() {
         this.$element.on("click", ".swipemod-button", event => this.onButtonClick(event) );
 
         this.$element.on("click", ".swipemod-item", event => this.onItemClick(event) );
-
     }
 
     onButtonClick(event) {
         let $btn = $(event.currentTarget),
             direction = $btn.data("direction");
 
-        if (direction === "left") {
+        if (direction === DIRECTION.LEFT) {
             this.scrollLeft();
-        } else if (direction === "right") {
+        } else if (direction === DIRECTION.RIGHT) {
             this.scrollRight();
         }
     }
@@ -50,15 +57,47 @@ export class SrfSwiper {
     }
 
     /**
+     * We can scroll to the left as long as the first item is "too far left".
+     *
+     * @return {Boolean}
+     */
+    canScrollLeft() {
+        return this.isOutOfBoundsLeft( this.$items.first() );
+    }
+
+    /**
+     * We can scroll to the right as long as the last item is "too far right".
+     *
+     * @return {Boolean}
+     */
+    canScrollRight() {
+        return this.isOutOfBoundsRight( this.$items.last() );
+    }
+
+    showHidePrevNextButtons() {
+        let showLeft = false,
+            showRight = false;
+
+
+        if (this.isSize2()) {
+            showLeft = this.canScrollLeft();
+            showRight = this.canScrollRight();
+        }
+
+        this.$prevBtn.toggle( showLeft );
+        this.$nextBtn.toggle( showRight );
+    };
+
+    /**
      * An element is inside its parent's boundaries if it doesn't go over the left or right side.
      * @param $itemElem
      * @return {boolean}
      */
     isItemCompletelyInView($itemElem) {
-        return !this.isOufOfBoundsLeft($itemElem) && !this.isOutOfBoundsRight($itemElem);
+        return !this.isOutOfBoundsLeft($itemElem) && !this.isOutOfBoundsRight($itemElem);
     }
 
-    isOufOfBoundsLeft($itemElem) {
+    isOutOfBoundsLeft($itemElem) {
         return $itemElem.offset().left < this.$container.offset().left;
     }
 
@@ -70,40 +109,50 @@ export class SrfSwiper {
     }
 
     scrollLeft() {
-        $(this.$element.find(".swipemod-item").get().reverse()).each( (index, el) => {
-            if ( this.isOufOfBoundsLeft( $(el) ) ) {
+        $(this.$items.get().reverse()).each( (index, el) => {
+            if ( this.isOutOfBoundsLeft( $(el) ) ) {
                 this.scrollItemIntoView( $(el) );
                 return false;
             }
         });
+
+        this.showHidePrevNextButtons();
     }
 
     scrollRight() {
-        this.$element.find(".swipemod-item").each( (index, el) => {
+        this.$items.each( (index, el) => {
             if ( this.isOutOfBoundsRight( $(el) ) ) {
                 this.scrollItemIntoView( $(el) );
                 return false;
             }
         });
+
+        this.showHidePrevNextButtons();
     }
 
     scrollItemIntoView($itemElem) {
         let currentScrollPos = this.$swipeContainer.scrollLeft(),
             newScrollPos = currentScrollPos;
 
-        if (this.isOufOfBoundsLeft($itemElem)) {
+        if (this.isOutOfBoundsLeft($itemElem)) {
             newScrollPos -= this.$container.offset().left - $itemElem.offset().left;
-            newScrollPos -= this.$containerPadding;
+            newScrollPos -= this.containerPadding;
         } else {
             let rightEdgeItem = $itemElem.offset().left + $itemElem.outerWidth(),
                 rightEdgeContainer = this.$container.offset().left + this.$container.outerWidth();
 
             newScrollPos += rightEdgeItem - rightEdgeContainer;
-            newScrollPos += this.$containerPadding;
+            newScrollPos += this.containerPadding;
         }
 
         this.$swipeContainer.animate({
             scrollLeft: newScrollPos
-        }, animationSpeed);
+        }, animationSpeed, () => {
+            this.showHidePrevNextButtons();
+        });
+    }
+
+    isSize2() {
+        return window.innerWidth >= BREAKPOINT_2;
     }
 }
