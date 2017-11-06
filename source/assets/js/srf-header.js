3b93064f66d28b49f27e4c27cc84d4fb6cb0d800
Philipp Christen <philipp.christen@srf.ch>
2017-11-06 17:10:18 +0100

    A11Y improvements in header/menu
    SRFCMSAL-362

--- source/assets/js/srf-header.js
+++ source/assets/js/srf-header.js
@@ -22,36 +22,52 @@ export class SrfHeader {
 
     constructor(element, options) {
         this.$element = $(element);
+        this.menuToggleCallback  = this.checkFunctionParam(options.menuToggleCallback);
+        this.submenuToggleCallback  = this.checkFunctionParam(options.submenuToggleCallback);
+
         this.$menuButton = this.$element.find(".js-menu-button");
         this.$subMenuButton = this.$element.find(".js-expand-arrow");
-
         this.menuIsOpen = false;
-        this.menuToggleCallback  = options.menuToggleCallback && typeof options.menuToggleCallback == 'function' ? options.menuToggleCallback : () => {};
-        this.submenuToggleCallback  = options.submenuToggleCallback && typeof options.submenuToggleCallback == 'function' ? options.submenuToggleCallback : () => {};
 
         // Submenu (Radio)
         this.$subMenuContent = $(".navigation__group--radio");
         this.$arrow = $(".expand-arrow");
 
+        // A11Y
+        this.$logo = this.$element.find(".header-startlink");
+        this.$navigation = this.$element.find(".js-header-navigation");
+        this.$A11YElements = $("body > div, body > section, body > footer").not(".header");
+        this.setA11YProperties(this.menuIsOpen);
+
+
         this.registerListeners();
+    }
+
+    /**
+     * Make sure a parameter is actually a function - return an empty function if it's not.
+     *
+     * @param param any
+     * @return {function()}
+     */
+    checkFunctionParam(param) {
+        return param && typeof param == 'function' ? param : () => {};
     }
 
     registerListeners() {
         this.$menuButton.on("click", event => this.onMenuButtonClicked(event) );
+        this.$subMenuButton.on("click", event => this.onSubMenuButtonClicked(event) );
 
         $(document).on("click", event => this.onDocumentClicked(event) );
-        $(document).on("keydown", event => this.onKeyPressed(event) );
-
-        this.$subMenuButton.on("click", event => this.onSubMenuButtonClicked(event) );
     }
 
     /**
-     * Click on any element outside of the header should close the menu if it's open.
+     * Click on any element outside of the header or on the root element of the header itself should close the menu if
+     * it's open.
      *
-     * @param e jQuery.event
+     * @param e {jQuery.Event}
      */
     onDocumentClicked(e) {
-        if (this.menuIsOpen && !$.contains(this.$element[0], e.target) || $(e.target).hasClass("js-close-menu") ) {
+        if (this.menuIsOpen && !$.contains(this.$element[0], e.target)) {
             this.close();
         }
     }
@@ -70,7 +86,14 @@ export class SrfHeader {
 
         $('html').toggleClass("menu--opened", this.menuIsOpen);
 
+        this.setA11YProperties(this.menuIsOpen);
         this.menuToggleCallback(this.menuIsOpen);
+
+        if (this.menuIsOpen) {
+            $(document).on("keydown.header", event => this.onKeyPressed(event));
+        } else {
+            $(document).off("keydown.header");
+        }
     }
 
     onSubMenuButtonClicked(e) {
@@ -85,14 +108,43 @@ export class SrfHeader {
         this.submenuToggleCallback(subMenuIsOpen);
     }
 
+    /**
+     * Multiple key events concern us:
+     * - Escape if the menu's open --> close it
+     * - trying to Tab out of the menu --> re-focus on the beginning
+     * - Key events on any element outside of the header --> close the menu
+     *
+     * @param e {jQuery.Event}
+     */
     onKeyPressed(e) {
-        if (e.keyCode === KEYCODES.escape && this.menuIsOpen ) {
+        if (e.keyCode === KEYCODES.escape ) {
+            this.close();
+        } else if (e.keyCode === KEYCODES.tab && this.$element.find(".navigation__link").last().is(e.target)) {
+            this.$logo.focus();
+        } else if (!$.contains(this.$element[0], e.target)) {
             this.close();
         }
     }
 
     close() {
         this.changeMenuState(false);
+    }
+
+    /**
+     * When the menu is open, make the navigation accessible to screenreaders and hide the rest of the page from them.
+     *
+     * @param menuIsOpened {Boolean}
+     */
+    setA11YProperties(menuIsOpened) {
+        this.$navigation.attr({
+            "aria-hidden": !menuIsOpened,
+            "role": menuIsOpened ? "" : "presentation"
+        });
+
+        this.$A11YElements.attr({
+            "aria-hidden": menuIsOpened,
+            "role": menuIsOpened ? "presentation": ""
+        });
     }
 }
 
