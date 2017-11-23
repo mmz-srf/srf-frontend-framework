71f1c4470457caf00fde2dfba1ed2c62f8317849
Manuel Hartmann <manuel.hartmann@ymc.ch>
2017-11-23 14:19:28 +0100

    Fix selector
    SRFCMSAL-480

--- source/assets/js/srf-comments.js
+++ source/assets/js/srf-comments.js
@@ -13,7 +13,7 @@ let commentController = function () {
 
     this.init = function () {
 
-        $('.js-comment-count-up').on('keyup focus', '.reply__textarea', function (e) {
+        $('.js-article-comments').on('keyup focus', '.reply__textarea', function (e) {
             that.countChars($(this));
         }).on('focusin', '.reply__textarea', function () {
             $('.reply').addClass('reply--on-focus');
@@ -28,7 +28,7 @@ let commentController = function () {
 
     this.countChars = function ($textarea) {
         let len = $textarea.val().length
-            , $button = $('.js-comment-count-up .button');
+            , $button = $('.js-article-comments .button');
 
         // count up available chars
         $('.js-comment-count-up').text(this.max_input - len);
