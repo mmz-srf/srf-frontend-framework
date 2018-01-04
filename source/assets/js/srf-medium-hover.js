5646896e00af4cfd1d0fabf2a1c9e5808400afee
Roman Hatz <roman.hatz@srf.ch>
2018-01-04 16:20:12 +0100

    fix selector
    SRFCMSDA-878

--- source/assets/js/srf-medium-hover.js
+++ source/assets/js/srf-medium-hover.js
@@ -2,21 +2,22 @@
  * This file handles the hover state of a medium molecule.
  * It can also be used on a media-still molecule if it is used as a standalone (=not a child of a medium) molecule.
  */
+const HOOK_SELECTOR = '.js-medium-hover';
 
 export function init() {
     // making sure that the binding only occurs once per molecule
-    $('.js-medium-hover .js-medium-hover').removeClass('js-medium-hover');
+    $(HOOK_SELECTOR + ' ' + HOOK_SELECTOR).removeClass(HOOK_SELECTOR);
 
     // hover for medium element
-    $(document).on("mouseenter", '.js-medium-hover', (event) => {
-        let $element = $(event.target);
+    $(document).on("mouseenter", HOOK_SELECTOR, (event) => {
+        let $element = $(event.target.closest(HOOK_SELECTOR));
         $element.find('.media-caption').addClass('media-caption--hover');
         $element.find('.play-icon-ng').addClass('play-icon-ng--hover');
         $element.find('.media-still__image').addClass('media-still__image--hover');
     });
 
-    $(document).on("mouseleave", '.js-medium-hover', (event) => {
-        let $element = $(event.target);
+    $(document).on("mouseleave", HOOK_SELECTOR, (event) => {
+        let $element = $(event.target.closest(HOOK_SELECTOR));
         $element.find('.media-caption').removeClass('media-caption--hover');
         $element.find('.play-icon-ng').removeClass('play-icon-ng--hover');
         $element.find('.media-still__image').removeClass('media-still__image--hover');
