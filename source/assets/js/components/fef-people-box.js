9ead275a1af5d1504e039a5316438b5ef7ca5b88
Philipp Christen <philipp.christen@srf.ch>
2018-01-25 16:05:40 +0100

    Accessibility fixes for the people/person box
    SRFCMSAL-559

--- source/assets/js/components/fef-people-box.js
+++ source/assets/js/components/fef-people-box.js
@@ -4,33 +4,61 @@ $(document).ready(() => {
     });
 });
 
+const ANIMATIONDURATION = 200;
+const KEYCODES = {
+    'enter': 13,
+    'space': 32
+};
+
 export class FefPeopleBox {
 
     /**
-     * @param $element jQuery element
+     * @param $element jQuery.element
      */
     constructor ($element) {
+        this.$element = $element;
         this.$arrow = $element.find(".expand-icon");
         this.$body = $element.find(".js-people-body");
+        this.$header = $element.find(".js-people-header");
 
-        this.bindEvents($element);
+        this.bindEvents();
     }
 
     /**
-     * @param $element
+     * Bind click and keydown event so that the people box can be opened/closed.
      */
-    bindEvents ($element) {
-        $element.find(".js-people-header").on('click', () => {
-            let willBeShown = !$element.hasClass("people--expanded");
+    bindEvents () {
+        this.$header.on('click', (event) => {
+            this.toggleBox(event)
+            this.$header.blur();
+        });
 
-            $element.toggleClass("people--expanded", willBeShown);
-            this.$arrow.toggleClass('expand-icon--open', !willBeShown);
-
-            if (willBeShown) {
-                this.$body.slideDown(300);
-            } else {
-                this.$body.slideUp(300);
+        this.$header.on('keydown', (event) => {
+            if (event.keyCode === KEYCODES.enter || event.keyCode === KEYCODES.space) {
+                this.toggleBox(event);
             }
         });
+    }
+
+    /**
+     * Opens or closes the content of the box. This is done via jQuery's slideUp/-Down because
+     * the height is dynamic.
+     *
+     * @param event
+     */
+    toggleBox(event) {
+        let willBeShown = !this.$element.hasClass("people--expanded");
+
+        this.$element.toggleClass("people--expanded", willBeShown);
+        this.$arrow.toggleClass('expand-icon--open', willBeShown);
+
+        if (willBeShown) {
+            this.$body.slideDown(ANIMATIONDURATION);
+        } else {
+            this.$body.slideUp(ANIMATIONDURATION);
+        }
+
+        event.preventDefault();
+        event.stopPropagation();
     }
 }
