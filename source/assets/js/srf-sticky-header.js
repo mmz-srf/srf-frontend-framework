96ab75373b2f5eacc2e0ba5c300b91149f2f170c
mms-caro <caroline.ast@srf.ch>
2018-04-05 18:07:03 +0200

    sticky header - transitions...
    SRFCMSAL-857

--- source/assets/js/srf-sticky-header.js
+++ source/assets/js/srf-sticky-header.js
@@ -4,14 +4,18 @@ export function init() {
     });
 }
 
+export const MASTHEAD_PADDING_BOTTOM = 30;
+
 export class SrfStickyHeader {
 
     constructor(element, options) {
         this.$masthead = $(element);
         this.$mastheadNav = $('.masthead__nav');
+        this.$stickyContainer = this.$masthead.closest('.sticky-container');
         this.affixMarginTop = this.$masthead.height() - this.$mastheadNav.height();
         this.$affixPlacehoder = $('.affix-placeholder');
-        this.affixPlacehoderHeight = this.$masthead.height() + 30;
+        this.affixPlacehoderHeight = this.$masthead.height() + MASTHEAD_PADDING_BOTTOM;
+        this.isAffixTop = false;
 
         this.initializeAffix();
         this.registerListeners();
@@ -23,28 +27,50 @@ export class SrfStickyHeader {
         let lastScrollTop = 0;
 
         $(window).scroll(function() {
+
             if(debounceTimer) {
                 window.clearTimeout(debounceTimer);
             }
             debounceTimer = window.setTimeout(function() {
-                let st = $(this).scrollTop();
-                if (st < lastScrollTop) {
+
+                let scrollTop = $(this).scrollTop();
+
+                if (that.$stickyContainer.hasClass('affix')) {
+                    that.isAffixTop = false;
+                    that.$stickyContainer.css('transition', 'margin-top 300ms linear');
+                } else {
+                    that.isAffixTop = true;
+                    that.$stickyContainer.css('margin-top', '');
+                    that.$stickyContainer.css('transition', 'none');
+                }
+
+                if (scrollTop <= lastScrollTop) {
+                    // scroll up
+
+                    if (that.isAffixTop) {
+                        that.$stickyContainer.css('position', 'fixed');
+                    }
+
                     $('.affix').css('margin-top', '0');
                 } else {
+                    // scroll down
+
+                    if (that.isAffixTop) {
+                        that.$stickyContainer.css('position', 'absolute');
+                    } else {
+                        that.$stickyContainer.css('position', 'fixed');
+                    }
+
                     $('.affix').css('margin-top', '-' + that.affixMarginTop + 'px');
                 }
-                lastScrollTop = st;
+                lastScrollTop = scrollTop;
             }, 10);
         });
     }
 
     initializeAffix() {
-        $("[data-smart-affix]").affix({offset:{top: this.getCorrectOffset()}});
+        $("[data-smart-affix]").affix({offset:{top: this.affixMarginTop}});
         this.$affixPlacehoder.css('height', this.affixPlacehoderHeight + 'px');
-    }
-
-    getCorrectOffset() {
-        return $("[data-smart-affix-placeholder]").offset().top + $(window).scrollTop();
     }
 
 }
