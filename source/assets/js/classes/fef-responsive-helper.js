3c7bb12c6c511fa508073f8cafc49fe10d0cdbea
Patrick Bonetti <patrick.bonetti@srf.ch>
2018-04-12 17:41:54 +0200

    showing pager icons and sub navi masks depending on breakpoint and navigation position
    SRFCMSAL-857

---
+++ source/assets/js/classes/fef-responsive-helper.js
@@ -0,0 +1,48 @@
+export class FefResponsiveHelper {
+
+    /**
+     * Returns true if current breakpoint is 'smartphone'
+     *
+     * @returns {boolean}
+     */
+    static isSmartphone() {
+        return this.getBreakpoint() === 'smartphone';
+    }
+
+    /**
+     * Returns true if current breakpoint is 'tablet'
+     *
+     * @returns {boolean}
+     */
+    static isTablet() {
+        return this.getBreakpoint() === 'tablet';
+    }
+
+    /**
+     * Returns true if current breakpoint is 'desktop'
+     *
+     * @returns {boolean}
+     */
+    static isDesktop() {
+        return this.getBreakpoint() === 'desktop';
+    }
+
+    /**
+     * Returns true if current breakpoint is 'desktop-wide'
+     *
+     * @returns {boolean}
+     */
+    static isDesktopWide() {
+        return this.getBreakpoint() === 'desktop-wide';
+    }
+
+    /**
+     * Returns the content of the body::before pseudo-element which contains the
+     * breakpoint string set via css media queries in _breakpoints.scss
+     *
+     * @returns {string}
+     */
+    static getBreakpoint() {
+        return window.getComputedStyle(document.querySelector('body'), '::before').getPropertyValue('content').replace(/\"/g, '');
+    }
+}
