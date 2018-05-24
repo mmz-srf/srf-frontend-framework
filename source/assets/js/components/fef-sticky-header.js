c6c00f3e729644a4a70bc6b0a6f07555157b2f02
Patrick Bonetti <patrick.bonetti@srf.ch>
2018-05-24 17:41:28 +0200

    branded header stays branded on scrolling; padding of masthead on smartphone fixed
    SRFCMSAL-1042

--- source/assets/js/components/fef-sticky-header.js
+++ source/assets/js/components/fef-sticky-header.js
@@ -14,8 +14,6 @@ export class FeFStickyHeader {
 
     constructor(element) {
         this.$masthead = $(element);
-        this.$mastheadWrapper = $('.masthead__wrapper', this.$masthead);
-        this.$mastheadBackground = $('.masthead__background', this.$masthead);
         this.$mastheadNav = $('.masthead__nav', this.$masthead);
 
         this.initializeAffix();
@@ -27,18 +25,13 @@ export class FeFStickyHeader {
         // Do not initialize in case of home landingpage on a breakpoint larger then smartphone
         if(!(!FefResponsiveHelper.isSmartphone() && this.$masthead.hasClass('masthead--home'))) {
             $(AFFIX_SELECTOR).affix({offset:{top: this.getAffixMarginTop()}});
-
-            // register branding transition animation only on branded headers
-            if(this.$masthead[0].className.includes('masthead--theme')) {
-                $(window).off('affix.bs.affix affix-top.bs.affix').on('affix.bs.affix affix-top.bs.affix', () => this.doBrandingTransition());
-            }
         }
     }
 
     reInitializeAffix() {
         // make sure that the affix is not registered twice
         $(window).off('.affix');
-        $(AFFIX_SELECTOR).removeClass("affix affix-top").removeData("bs.affix");
+        $(AFFIX_SELECTOR).removeClass('affix affix-top').removeData('bs.affix');
 
         // initialize the affix
         this.initializeAffix();
@@ -46,18 +39,9 @@ export class FeFStickyHeader {
 
     getAffixMarginTop() {
         if (this.$mastheadNav.outerHeight() !== undefined) {
-            return this.$masthead.outerHeight() - this.$mastheadNav.outerHeight();
+            return this.$masthead.outerHeight() - this.$mastheadNav.outerHeight() ;
         } else {
             return this.$masthead.outerHeight();
         }
-    }
-
-    doBrandingTransition() {
-        this.$mastheadWrapper.animate ({ opacity: 0 }, 100);
-        this.$mastheadBackground.animate ({ opacity: 0 }, 150, () => {
-            this.$masthead.toggleClass('masthead--overridden');
-            this.$mastheadWrapper.animate ({ opacity: 1 }, 100);
-            this.$mastheadBackground.animate ({ opacity: 1 }, 100);
-        });
     }
 }
