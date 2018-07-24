6445f017a19d906954fd08e9d32e470f2cb00206
mms-caro <caroline.ast@srf.ch>
2018-07-24 11:46:03 +0200

    set correct aria attributes for subnav
    SRFCMSAL-999

--- source/assets/js/components/fef-subnav.js
+++ source/assets/js/components/fef-subnav.js
@@ -127,8 +127,13 @@ export class FefSubnav {
         this.closeAllSubNavs();
 
         if (isClosing) {
+            $(e.target).attr('aria-expanded', false);
+            $(e.target).attr('aria-haspopup', false);
             return;
         }
+
+        $(e.target).attr('aria-expanded', true);
+        $(e.target).attr('aria-haspopup', true);
 
         this.$element.addClass('subnav--open-3rd-level');
 
