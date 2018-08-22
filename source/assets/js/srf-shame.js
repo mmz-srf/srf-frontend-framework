605a42436210a9351cb0efd1d5039a5f3baa22a9
Philipp Christen <philipp.christen@srf.ch>
2018-08-22 18:04:42 +0200

    make IE shame stuff more resilient
    noref

--- source/assets/js/srf-shame.js
+++ source/assets/js/srf-shame.js
@@ -29,7 +29,7 @@ function objectFitForIE() {
         getObjectFitValue = elem => {
             let objectFitVal = 'contain';
 
-            if ( elem.currentStyle ) {
+            if (  elem.currentStyle ) {
                 objectFitVal = elem.currentStyle.getAttribute('object-fit');
             }
 
@@ -64,8 +64,14 @@ function objectFitForIE() {
             containers = document.querySelectorAll( relevantClasses.join(', ') );
 
         for (let i = 0; i < containers.length; i++) {
-            let oldImg = containers[i].querySelector('img'),
-                objectFitVal = getObjectFitValue(oldImg);
+            let oldImg = containers[i].querySelector('img');
+
+            // only do something if there's an image.
+            if (!oldImg) {
+                return;
+            }
+
+            let objectFitVal = getObjectFitValue(oldImg);
 
             if (objectFitVal === 'contain' || objectFitVal === 'cover') {
 
