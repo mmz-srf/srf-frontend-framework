2364d027b785d0b0a5bf9f78490d2e7a8b58b30a
Philipp Denzler <philipp.denzler@srf.ch>
2018-10-08 16:23:40 +0200

    Add TouchEvent and touch count detection to FefTouchDetection

--- source/assets/js/classes/fef-touch-detection.js
+++ source/assets/js/classes/fef-touch-detection.js
@@ -9,4 +9,24 @@ export class FefTouchDetection {
         return ('ontouchstart' in window) || typeof window.DocumentTouch !== 'undefined' && document instanceof DocumentTouch;
     }
 
+    /**
+     * Detects if the given event is a TouchEvent.
+     *
+     * @returns {boolean}
+     */
+    static isTouchEvent(event) {
+        const originalEvent = event.originalEvent ? event.originalEvent : event;
+        return typeof originalEvent.touches !== 'undefined';
+    }
+
+    /**
+     * Returns the number of touch points if the given event is a TouchEvent.
+     *
+     * @returns {boolean}
+     */
+    static getTouchCount(event) {
+        const originalEvent = event.originalEvent ? event.originalEvent : event;
+        return isTouchEvent(originalEvent) ? originalEvent.touches.length : 0;
+    }
+
 }
