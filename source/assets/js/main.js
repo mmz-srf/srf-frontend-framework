d2086fcd7efcb63671cf961a03889f11007aa467
Pascal von Bueren <pascal.von.bueren@ymc.ch>
2019-04-02 13:57:43 +0200

    disable flying focus for now

--- source/assets/js/main.js
+++ source/assets/js/main.js
@@ -38,7 +38,7 @@ document.addEventListener('DOMContentLoaded', function(event) {
     FefGlobalnavInit();
     FefSwipeableAreaInit();
     FefSwipeModuleInit();
-    FefFlyingFocusInit();
+    //FefFlyingFocusInit();
     FefExpandableBoxInit();
     FefStickyHeaderInit();
     FefSubnavInit();
