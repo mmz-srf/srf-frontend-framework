04111071bec65ddf587adc5d61ae71b4e74105eb
Stefan Rechsteiner <info@stefanrechsteiner.com>
2019-07-11 15:39:19 +0200

    linting
    SRFCMSAL-2500

--- source/assets/js/srf-a2z.js
+++ source/assets/js/srf-a2z.js
@@ -3,31 +3,31 @@ export function init() {
     let triggers = document.querySelectorAll('.js-filter-bar-trigger');
     for (let i = 0; i < triggers.length; i++) {
         let currentTrigger = triggers[i];
-        currentTrigger.addEventListener('click', function(){
+        currentTrigger.addEventListener('click', function() {
 
             const blockID = this.getAttribute('data-blockid'),
-                  allID = 'a2z-all',
-                  letterClass = 'filter-bar__letter',
-                  letterActiveClass = 'filter-bar__letter--active',
-                  blockClass = 'a2z-lists__block',
-                  hiddenClass = 'a2z-lists__block--hidden';
+                allID = 'a2z-all',
+                letterClass = 'filter-bar__letter',
+                letterActiveClass = 'filter-bar__letter--active',
+                blockClass = 'a2z-lists__block',
+                hiddenClass = 'a2z-lists__block--hidden';
 
             // styles clicked filter-bar-Element as active
-            document.querySelectorAll('.' + letterClass).forEach(function(thisNode){
+            document.querySelectorAll('.' + letterClass).forEach(function(thisNode) {
                 thisNode.classList.remove(letterActiveClass);
             });
             this.classList.add(letterActiveClass);
 
             // show/hide selected Block of Elements
             if (blockID == allID) {
-                document.querySelectorAll('.' + hiddenClass).forEach(function(thisNode){
+                document.querySelectorAll('.' + hiddenClass).forEach(function(thisNode) {
                     thisNode.classList.remove(hiddenClass);
                 });
             } else {
-                document.querySelectorAll('.' + blockClass + '#' + blockID).forEach(function(thisNode){
+                document.querySelectorAll('.' + blockClass + '#' + blockID).forEach(function(thisNode) {
                     thisNode.classList.remove(hiddenClass);
                 });
-                document.querySelectorAll('.' + blockClass + ':not(#' + blockID + ')').forEach(function(thisNode){
+                document.querySelectorAll('.' + blockClass + ':not(#' + blockID + ')').forEach(function(thisNode) {
                     thisNode.classList.add(hiddenClass);
                 });
             }
