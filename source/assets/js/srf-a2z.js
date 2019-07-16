cd4fb62eb05eb81337fed6534de9c791c022cb57
Manuel Hartmann <manuel.hartmann@ymc.ch>
2019-07-16 11:32:02 +0200

    Refactor letter filter
    SRFCMSAL-2500

--- source/assets/js/srf-a2z.js
+++ source/assets/js/srf-a2z.js
@@ -3,42 +3,78 @@ const CONTAINER_LETTER_BOX = '.a2z-lists__block';
 const CONTAINER_LETTER_BOX_ENTRY = '.js-a2z-filter';
 const CONTAINER_FILTER_BAR = '.js-filter-bar';
 const CONTAINER_FILTER_BAR_LETTER = '.js-a2z-filter-bar-letter';
+
 const FILTER_SELECT = '.js-select-menu';
-const CLASS_HIDE = 'a2z--hidden';
+
+const CLASS_ACTIVATE_FILTER_LETTERS = 'filter-bar__letter--active';
 const CLASS_HIDE_FILTER_LETTERS = 'filter-bar__letter--inactive';
+const CLASS_HIDE_LETTER_BOX = 'a2z-lists__block--hidden';
+const CLASS_HIDE_TEASER = 'pseudo-table__row--hidden';
+
 
 export class A2zFilter {
 
-    constructor($container) {
-        this.$container = $container;
+    constructor() {
         this.startObserver();
     }
 
     startObserver() {
-        this.$container.on('change', FILTER_SELECT, (event) => {
+        $(FILTER_SELECT).on('change', (event) => {
             event.preventDefault();
-
-            this.resetFilters();
+            this.resetFiltersForSelectBoxFilter();
+            $(`${CONTAINER_FILTER_BAR_LETTER}[data-blockid="a2z-all"]`).addClass(CLASS_ACTIVATE_FILTER_LETTERS);
             if ($(event.target).val() !== 'all') {
                 this.filterTeasersByChannelId($(event.target).val());
             }
             this.toggleLettersAndLetterBoxes();
         });
+
+
+        $(`${CONTAINER_FILTER_BAR_LETTER}:not([data-blockid="a2z-all"])`).on('click', (event) => {
+            event.preventDefault();
+            if (!$(event.target).hasClass(CLASS_HIDE_FILTER_LETTERS)) {
+                this.resetLetterBoxFilter();
+                $(event.target).addClass(CLASS_ACTIVATE_FILTER_LETTERS);
+                this.hideLetterBoxesExpectId($(event.target).data('blockid'));
+            }
+        });
+
+        $(`${CONTAINER_FILTER_BAR_LETTER}[data-blockid="a2z-all"]`).on('click', (event) => {
+            event.preventDefault();
+            this.resetLetterBoxFilter();
+            $(event.target).addClass(CLASS_ACTIVATE_FILTER_LETTERS);
+        });
+
     }
 
-    resetFilters() {
-        $(`.${CLASS_HIDE}`).removeClass(CLASS_HIDE);
+    resetFiltersForSelectBoxFilter() {
+        this.resetLetterBoxFilter();
+        this.resetLetterFilter();
+        this.resetTeaserFilter();
+    }
+
+    resetLetterFilter() {
         $(`${CONTAINER_FILTER_BAR} ${CONTAINER_FILTER_BAR_LETTER}`).addClass(CLASS_HIDE_FILTER_LETTERS);
     }
 
+    resetLetterBoxFilter() {
+        $(`.${CLASS_HIDE_LETTER_BOX}`).removeClass(CLASS_HIDE_LETTER_BOX);
+        $(`.${CLASS_ACTIVATE_FILTER_LETTERS}`).removeClass(CLASS_ACTIVATE_FILTER_LETTERS);
+    }
+
+    resetTeaserFilter() {
+        $(`.${CLASS_HIDE_TEASER}`).removeClass(CLASS_HIDE_TEASER);
+    }
+
+
     filterTeasersByChannelId(filterValue) {
-        $(`${CONTAINER_LETTER_BOX_ENTRY}:not([data-filter*="${filterValue}"])`).addClass(CLASS_HIDE);
+        $(`${CONTAINER_LETTER_BOX_ENTRY}:not([data-filter*="${filterValue}"])`).addClass(CLASS_HIDE_TEASER);
     }
 
     toggleLettersAndLetterBoxes() {
         $(CONTAINER_LETTER_BOX).each(function () {
             if($(this).find(`${CONTAINER_LETTER_BOX_ENTRY}:visible`).length === 0) {
-                $(this).addClass(CLASS_HIDE);
+                $(this).addClass(CLASS_HIDE_TEASER);
             } else {
                 $(`${CONTAINER_FILTER_BAR} ${CONTAINER_FILTER_BAR_LETTER}[data-blockid="${$(this).data('block')}"]`).removeClass(CLASS_HIDE_FILTER_LETTERS);
                 $(`${CONTAINER_FILTER_BAR} ${CONTAINER_FILTER_BAR_LETTER}[data-blockid="a2z-all"]`).removeClass(CLASS_HIDE_FILTER_LETTERS);
@@ -46,59 +82,23 @@ export class A2zFilter {
         });
     }
 
-    loadImages() {
+    hideLetterBoxesExpectId(targetId) {
+        $(`${CONTAINER_LETTER_BOX}:not([data-block="${targetId}"])`).addClass(CLASS_HIDE_LETTER_BOX);
+    }
 
+    loadImages() {
+            // thisNode.querySelectorAll('img:not(.loaded)').forEach((imageNode) => {
+            //                     if (imageNode.hasAttribute('data-src')) {
+            //                         imageNode.classList.add('loaded');
+            //                         imageNode.src = imageNode.dataset.src;
+            //                     }
+            //                 });
     }
 }
 
 export function init() {
-
-    let $container = $(CONTAINER);
-    if ($container.length) {
-        let a2zFilter = new A2zFilter($container);
-    }
-
-
-    let triggers = document.querySelectorAll('.js-filter-bar-trigger');
-    for (let i = 0; i < triggers.length; i++) {
-        let currentTrigger = triggers[i];
-        currentTrigger.addEventListener('click', function(e) {
-            e.preventDefault();
-
-            const blockID = this.getAttribute('data-blockid'),
-                allID = 'a2z-all',
-                letterClass = 'filter-bar__letter',
-                letterActiveClass = 'filter-bar__letter--active',
-                blockClass = 'a2z-lists__block',
-                hiddenClass = 'a2z-lists__block--hidden';
-
-            // styles clicked filter-bar-Element as active
-            document.querySelectorAll('.' + letterClass).forEach(function(thisNode) {
-                thisNode.classList.remove(letterActiveClass);
-            });
-            this.classList.add(letterActiveClass);
-
-            // show/hide selected Block of Elements
-            if (blockID === allID) {
-                document.querySelectorAll('.' + hiddenClass).forEach(function(thisNode) {
-                    thisNode.classList.remove(hiddenClass);
-                });
-            } else {
-                document.querySelectorAll('.' + blockClass + '[data-block="' + blockID + '"]').forEach(function(thisNode) {
-                    thisNode.classList.remove(hiddenClass);
-                    thisNode.querySelectorAll('img:not(.loaded)').forEach((imageNode) => {
-                        if (imageNode.hasAttribute('data-src')) {
-                            imageNode.classList.add('loaded');
-                            imageNode.src = imageNode.dataset.src;
-                        }
-                    });
-                });
-                document.querySelectorAll('.' + blockClass + ':not([data-block="' + blockID + '"])').forEach(function(thisNode) {
-                    thisNode.classList.add(hiddenClass);
-                });
-            }
-
-        });
+    if ($(CONTAINER).length) {
+        new A2zFilter();
     }
 }
 
