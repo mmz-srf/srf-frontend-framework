cea005f99c7f867b011d74e05f5d22a683c5472d
mms-caro <caroline.ast@srf.ch>
2019-10-30 11:34:16 +0100

    review feedback
    SRFCMSAL-2552

--- source/assets/js/components/fef-a11y.js
+++ source/assets/js/components/fef-a11y.js
@@ -1,22 +1,27 @@
+export function init() {
+    enableSkiplinks();
+}
+
+
 /**
  * Some browsers do not fully support in-page links. While they may visually shift focus to the location
  * of the target or named anchor for the “skip” link, they do not actually set keyboard focus to this location.
  *
  * https://www.bignerdranch.com/blog/web-accessibility-skip-navigation-links/
  */
-
-
-export function init() {
+export function enableSkiplinks() {
     // flag link to inform about manipulation of the link
     $('.js-skiplink').attr('data-skiplink', 'true');
 
-    $('.js-skiplink').click(function() {
+    $('.js-skiplink').on('click', function() {
         let skipTo='#'+this.href.split('#')[1];
         setFocus($(skipTo));
     });
 }
 
 /**
+ * Sets the focus correct to the location of the target (not only visually).
+ *
  * @param $element jQuery.Element
  */
 export function setFocus($element) {
