1b12c73dd43212efcf67ad143bef852f4aa20590
pchr-srf <philipp.christen@srf.ch>
2020-01-03 11:40:33 +0100

    make listmodule.js idempotent by marking & checking elements
    SRFCMSAL-2844

--- source/assets/js/srf-listmodule.js
+++ source/assets/js/srf-listmodule.js
@@ -1,13 +1,25 @@
+import { DOM_MUTATION_EVENTS } from './utils/fef-events';
+
 export function init() {
-    $(document).ready(function() {
-        let listElements = $('.listing-item');
-        listElements.each(function () {
-            let mediaCaption = $(this).find('.media-caption__source');
-            if (mediaCaption.hasClass('media-caption__source--video') || mediaCaption.hasClass('media-caption__source--audio')) {
-                $(this).find('.infoline').html(mediaCaption.html());
-            } else {
-                $(this).find('.infoline-source').html(mediaCaption.html());
+    $(window).on(DOM_MUTATION_EVENTS, () => {
+        let $listElements = $('.listing-item');
+
+        $listElements.each(function () {
+            // only initialize each list module once
+            if ($(this).data('listmodule-initialized')) {
+                return;
             }
+
+            let $mediaCaption = $(this).find('.media-caption__source');
+
+            if ($mediaCaption.hasClass('media-caption__source--video') || $mediaCaption.hasClass('media-caption__source--audio')) {
+                $(this).find('.infoline').html($mediaCaption.html());
+            } else {
+                $(this).find('.infoline-source').html($mediaCaption.html());
+            }
+
+            // mark element, so that it won't be initialized again by this module
+            $(this).data('listmodule-initialized', true);
         });
     });
 }
